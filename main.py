5f5b32f7987801cc65e447ee2ef7f6ae853a4c8e
JuliRoPi01 <juliropi01@gmail.com>
2025-07-08 13:14:16 -0500

    improved sudoku solver, added sudoku.py with sudokus
    not yet capable of solving difficult sudokus, to be added: check if only one cell can have the number in columns and boxes

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
 from copy import deepcopy
+from sudokus import sudokus
 
-sudoku = [
-    [0,6,0,0,9,0,0,8,7],
-    [8,3,0,2,6,0,5,0,0],
-    [9,2,0,8,0,7,0,4,3],
-    [0,0,0,0,0,0,0,0,6],
-    [6,8,0,7,5,0,0,0,2],
-    [5,0,0,0,0,3,0,0,0],
-    [2,5,0,4,0,0,7,6,9],
-    [3,1,9,0,0,0,8,2,4],
-    [0,4,0,0,8,2,0,0,0]
-]
-
+sudoku = sudokus[3]
 NUMS = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 def sudoku_solver() -> None:
     sudoku_solving = []
+    tries = 0
     cols_nums: dict = get_cols_nums()
     rows_nums: dict = get_rows_nums()
     boxes_nums: dict = get_boxes_nums()
-    possible_nums = dict()
-    while sudoku != sudoku_solving:
+    while tries < 3:
         for row in range(9):
             not_row_nums: dict = rows_nums[row]["notinit"]
             for col in range(9):
                 if sudoku[row][col] == 0:
-                    ubication = f"{row}{col}"
+                    cell = f"{row}{col}"
                     row_nums: set = rows_nums[row]["init"]
                     col_nums: set = cols_nums[col]
-                    box: str = get_cell_box(ubication, boxes_nums)
+                    box: str = get_cell_box(cell, boxes_nums)
                     box_nums: set = boxes_nums[box]
                     used_nums: set = row_nums | col_nums | box_nums
                     cell_nums: list = list(NUMS - used_nums)
@@ -36,24 +26,28 @@ def sudoku_solver() -> None:
                         num = cell_nums[0]
                         add_num_to_sudoku(num, row, col, box, rows_nums, cols_nums, boxes_nums)
                     else:
-                        possible_nums[ubication] = cell_nums
                         for num in cell_nums:
-                            not_row_nums[num].append(ubication)
-
+                            if cell not in not_row_nums[num]:
+                                not_row_nums[num].append(cell)
+            
             not_row_nums_copy: dict = deepcopy(not_row_nums)
             for key_num in not_row_nums_copy.keys():
                 if len(not_row_nums_copy[key_num]) == 1:
-                    cell: str = not_row_nums_copy[key_num][0]
+                    cell: str = list(not_row_nums_copy[key_num])[0]
                     num: int = key_num
                     row: int = int(cell[0])
                     col: int = int(cell[1])
                     box = get_cell_box(cell, boxes_nums)
                     add_num_to_sudoku(num, row, col, box, rows_nums, cols_nums, boxes_nums)
-                    
-        sudoku_solving = sudoku[:]
-        
+
+        if sudoku == sudoku_solving:
+            tries += 1
+        else:
+            tries = 0
+        empty_nums_not_in_row(rows_nums)
+        sudoku_solving = deepcopy(sudoku)
+    
     print(rows_nums)
-    print(possible_nums)
     for r in sudoku:
         print(r)
 
@@ -63,8 +57,7 @@ def get_rows_nums() -> dict:
         nums_in_row: set = set()
         for num in sudoku[row]:
             if num != 0:
-                nums_in_row |= {num}
-
+                nums_in_row.add(num)
         not_in_row = NUMS - nums_in_row
         nums_not_in_row: dict = {num:[] for num in not_in_row}
         
@@ -78,7 +71,7 @@ def get_cols_nums() -> dict:
         for  row in range(9):
             num: int = sudoku[row][col]
             if num != 0:
-                nums |= {num}
+                nums.add(num)
         col_nums[col] = nums
     return col_nums
 
@@ -95,7 +88,7 @@ def get_boxes_nums() -> dict:
                 for c in range(col, col+3):
                     num: int = sudoku[r][c]
                     if num != 0:
-                        box_nums |= {num}
+                        box_nums.add(num)
                     box += f"{r}{c}-"
             col += 3
             boxes_nums[box] = box_nums
@@ -113,10 +106,16 @@ def get_cell_box(cell: str, boxes_nums: dict) -> str:
 
 def add_num_to_sudoku(num: int, row: int, col: int, box: str, rows_nums: dict, cols_nums: dict, boxes_nums: dict) -> None:
     sudoku[row][col] = num
-    rows_nums[row]["init"] |= {num}
-    cols_nums[col] |= {num}
-    boxes_nums[box] |= {num}
+    rows_nums[row]["init"].add(num)
+    cols_nums[col].add(num)
+    boxes_nums[box].add(num)
     del rows_nums[row]["notinit"][num]
+    cell = f"{row}{col}"
+
+def empty_nums_not_in_row(rows_nums) -> None:
+    for r in range(9):
+        for key in rows_nums[r]["notinit"].keys():
+            rows_nums[r]["notinit"][key] = []
 
 if __name__ == "__main__":
     sudoku_solver()
